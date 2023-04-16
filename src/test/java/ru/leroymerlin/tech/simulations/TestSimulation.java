ecf7e8f3e5c72f5e8b608e292bf008dab2a22e4a
Sergey Mezinov <sergey.mezinov@leroymerlin.ru>
2023-04-16 09:22:44 +0300

    improve simulation

--- src/test/java/ru/leroymerlin/tech/simulations/TestSimulation.java
+++ src/test/java/ru/leroymerlin/tech/simulations/TestSimulation.java
@@ -3,8 +3,7 @@ package ru.leroymerlin.tech.simulations;
 import io.gatling.javaapi.core.ScenarioBuilder;
 import io.gatling.javaapi.core.Simulation;
 
-import static io.gatling.javaapi.core.CoreDsl.atOnceUsers;
-import static io.gatling.javaapi.core.CoreDsl.scenario;
+import static io.gatling.javaapi.core.CoreDsl.*;
 import static ru.leroymerlin.tech.base.Protocol.httpProtocol;
 import static ru.leroymerlin.tech.scenario.FirstTestScenario.testScenario;
 import ru.tinkoff.gatling.influxdb.Annotations;
@@ -15,7 +14,16 @@ public class TestSimulation extends Simulation implements Annotations {
 
     {
         setUp(
-                scn.injectOpen(atOnceUsers(1)).protocols(httpProtocol)
+                scn.injectOpen(
+                        nothingFor(4),
+                        atOnceUsers(10),
+                        rampUsers(10).during(5),
+                        constantUsersPerSec(20).during(15),
+                        constantUsersPerSec(20).during(15).randomized(),
+                        rampUsersPerSec(10).to(20).during(10),
+                        rampUsersPerSec(10).to(20).during(10).randomized(),
+                        stressPeakUsers(1000).during(20) // 8
+                ).protocols(httpProtocol)
         );
     }
 }
